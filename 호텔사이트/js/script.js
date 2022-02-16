08daa7fd79bbfc30c570bfad5a7a3cb411095459
NAMGUNGSEULAH <tmfdk7224@naver.com>
2022-02-16 18:46:06 +0900

    3일차

--- 호텔사이트/js/script.js
+++ 호텔사이트/js/script.js
@@ -3,6 +3,8 @@
 // 이미지 슬라이드 적용
 // 문서 준비 이벤트
 $(function() {
+
+    // 이미지 슬라이드 적용
     $('.visual .slide').slick({
         arrows: false,    // 화살표
         dots: false,      // 닷츠
@@ -10,6 +12,21 @@ $(function() {
         autoplay: true,         // 자동재생
         autoplaySpeed: 4000,    // 재생시간
         pauseOnHover: false,    // 마우스 호버시 정지
-        pauseOnFocus: true,     // 포커스시 정지
+        pauseOnFocus: false    // 포커스시 정지
+    })
+
+    // 탭 메뉴
+    $('.tab a').on('click', function() {
+        // 클릭한 탭의 순서번호
+        let index = $('.tab a').index($(this))
+
+        $('.tabBox').removeClass('on')
+        $('.tabBox:eq(' + index + ')').addClass('on')
+    })
+
+    // scrolla 플러그인 이용한 - 스크롤 애니메이션 적용
+    $('.animate').scrolla({
+        mobile: true,           // 모바일 버전 활성화 여부
+        once: false             // 애니메이션을 한 번 할지 여부
     })
 })
